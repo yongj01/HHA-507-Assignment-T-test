1d086e2debb811cdf099dcb0912f35b6dd2af459
jeremy Yong <jeremy.yong@stonybrook.edu>
2021-10-28 12:29:37 -0400

    Edits to include ## descriptions

--- HHA 507 Assignment FINAL.py
+++ HHA 507 Assignment FINAL.py
@@ -8,13 +8,19 @@ Created on Wed Oct 27 21:56:16 2021
 
 import pandas as pd
 from scipy.stats import ttest_ind
+from numpy.random import seed
+from numpy.random import randn
+import scipy.stats as stats
+from scipy.stats import shapiro
+from matplotlib import pyplot
 
 
 
 diabetes = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Datasets/Diabetes/DB1_Diabetes/diabetic_data.csv')
 
-diabetes_small = diabetes.sample(100)
+## Retrieving a small sample of the dataframe to visualize collumn names 
 
+diabetes_small = diabetes.sample(100)
 
 ## Questions: 
     
@@ -33,13 +39,20 @@ ttest_ind(Female['time_in_hospital'], Male['time_in_hospital'])
 
 ## The results are Ttest statistic=9.542637042242887, pvalue=1.4217299655114968e-21
 
+## Since the p value is below 0.05, there is a significant difference between the sex (M:F) and number of days in the hospital.
+
+
 
 ## 2) Is there a difference between RACE (Caucasian and African American) and the number of days in hospital?
 
 ## race
 ## timeinhospital
 
+## I did a count of all the race categories
+
 diabetes['race'].value_counts()
+
+## I created two variables for Caucasian and AfricanAmerican
 
 Caucasian = diabetes[diabetes['race']=='Caucasian']
 AfricanAmerican = diabetes[diabetes['race']=='AfricanAmerican']
@@ -47,14 +60,21 @@ ttest_ind(Caucasian['time_in_hospital'], AfricanAmerican['time_in_hospital'])
 
 ## The results are Ttest statistic=-5.0610017032095325, pvalue=4.178330085585203e-07
 
+## Since the p value is below 0.05, there is a significant difference between the race (Caucasian and African American) and number of days in the hospital.
+
+
 ## 3) Is there a difference between RACE (Asian and African American) and the number of lab procedures performed?
 
 
+## I created a variable for Asian
+
 Asian = diabetes[diabetes['race']=='Asian']
 AfricanAmerican = diabetes[diabetes['race']=='AfricanAmerican']
-ttest_ind(Asian['time_in_hospital'], AfricanAmerican['time_in_hospital'])
+ttest_ind(Asian['num_lab_procedures'], AfricanAmerican['num_lab_procedures'])
 
-## The results are Ttest statistic=-4.193460125064539, pvalue=2.7592265975996794e-05
+## The results are Ttest_ind statistic=-3.9788715315360292, pvalue=6.948907528800307e-05
+
+## Since the p value is below 0.05, there is a significant difference between the race (Asian and African American) and number of lab procedures performed.
 
 
 
